0aeb3c1d42bbae7c2872f7055e45052a131c7ef4
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-15 10:46:21 -0500

    Finish project

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  patch "restaurants/:id", to: "restaurants#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
+
+  # resources :restaurants
 end
+
+
+
+# index
+# show
+# new
+# create
+# edit
+# update
+# delete
